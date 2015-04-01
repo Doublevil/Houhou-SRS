b08596e04e0d37a6d014ce8f3947f6dfdc791bbc
Doublevil <doublevil@hotmail.fr>
2015-04-01 20:08:02 +0200

    Fixed a minor issue with radical and srs settings

---
+++ Kanji.Interface/Converters/NullValueToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Kanji.Interface.Converters
+{
+    class NullValueToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool? targetNull = parameter as bool?;
+            if (targetNull != false)
+            {
+                return value == null ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            // Specifically given the parameter "false".
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
