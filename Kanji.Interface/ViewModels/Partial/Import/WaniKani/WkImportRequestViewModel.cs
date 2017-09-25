721208b3c73235646823a075af5a73be24062c1b
Shoaib Shakeel <shoaibshakeel381@yahoo.com>
2017-09-25 18:18:57 +0500

    Fix Wanikani Import Issue.

--- Kanji.Interface/ViewModels/Partial/Import/WaniKani/WkImportRequestViewModel.cs
+++ Kanji.Interface/ViewModels/Partial/Import/WaniKani/WkImportRequestViewModel.cs
@@ -177,8 +177,8 @@ namespace Kanji.Interface.ViewModels
                 }
                 if (_parent.ImportMode == WkImportMode.All || _parent.ImportMode == WkImportMode.Vocab)
                 {
-                    uri.Add(string.Format("https://www.wanikani.com/api/v1.2/user/{0}/vocabulary/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25", _parent.ApiKey));
-                    uri.Add(string.Format("https://www.wanikani.com/api/v1.2/user/{0}/vocabulary/26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50", _parent.ApiKey));
+                    string levels = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60";
+                    uri.Add(string.Format("https://www.wanikani.com/api/v1.2/user/{0}/vocabulary/{1}", _parent.ApiKey, levels));
                 }   
 
                 responses = new string[uri.Count];
